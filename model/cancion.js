bb8645c6f2b547b17b9a8cbc6d89e3ed67857e44
diazdelia23 <40668741+diazdelia23@users.noreply.github.com>
2019-10-23 18:38:37 -0600

    todo con base de datos y unitTesting

--- model/cancion.js
+++ model/cancion.js
@@ -1,19 +1,29 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/myappdatabase', {useUnifiedTopology:true, useNewUrlParser:true});
+mongoose.connect('mongodb://localhost/dbMusica', { useUnifiedTopology: true, useNewUrlParser: true });
 
 var cancionSchema = new Schema({
-    id: {type: Number, required: true},
-    cancion: {type: String, required: true},
-    artista: {type: String, required: true},
-    album: {type: String, required: true},
-    anio: {type: String, required: true},
-    genero: {type: String, required: true}
+    id: { type: Number, required: true },
+    cancion: { type: String, required: true },
+    artista: { type: String, required: true },
+    album: { type: String, required: true },
+    anio: { type: String, required: true },
+    genero: { type: String, required: true }
 })
 
 var Cancion = mongoose.model('Cancion', cancionSchema);
 
+
+const newCancion = Cancion({
+    id: 1,
+    cancion: 'Love on the brain',
+    artista: 'Rihanna',
+    album: 'Anti',
+    anio: '2016',
+    genero: 'pop'
+});
+newCancion.save(function (err) { });
 
 
 module.exports = Cancion;
