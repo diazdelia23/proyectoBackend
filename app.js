590d38b4aec1e22820869a3ec811585480bc5a99
diazdelia23 <40668741+diazdelia23@users.noreply.github.com>
2019-11-24 19:17:11 -0600

    con cambios de ambiente

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 
 //settings
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
